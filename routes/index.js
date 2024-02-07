9fbd93859315769b686ecd09b6e065306c03b800
KB-Toast <kevin.brusson@gmail.com>
2024-02-07 13:01:53 +0100

    fixing tests

--- routes/index.js
+++ routes/index.js
@@ -16,40 +16,8 @@ router.get('/movies', (req, res) => {
 
   fetch(url, options)
     .then((data) => data.json())
-    .then((movies) => res.json(movies))
+    .then((movies) => res.json({ movies: movies.results }))
     .catch((err) => console.error('error:' + err));
 });
-/*
-router.get('/movies/:movieId', (req, res) => {
-  const totalUrl = '';
-  const urlConfig = 'https://api.themoviedb.org/3/configuration';
-  const optionsConfig = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
 
-  fetch(urlConfig, optionsConfig)
-    .then((res) => res.json())
-    .then((json) => (totalUrl += json.images.base_url))
-    .catch((err) => console.error('error:' + err));
-
-  const url = `https://api.themoviedb.org/3/movie/${req.params.movieId}/images`;
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
-
-  fetch(url, options)
-    .then((data) => data.json())
-    .then((img) => console.log('images: ', img))
-    .catch((err) => console.error('error:' + err));
-  console.log('totalUrl: ', totalUrl);
-});
-*/
 module.exports = router;
